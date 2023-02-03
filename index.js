ebe5e7495298da73b86217a12d898146730dafa1
Nikita Lobanov <114199158+nikitalobanov12@users.noreply.github.com>
2023-02-03 09:32:34 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -15,3 +15,41 @@ const hiddenVideoObserver = new IntersectionObserver(function (entries, hiddenVi
 }
     , hiddenVideoOptions);
 hiddenVideoObserver.observe(hiddenVideo);
+
+
+//for all the elements with the class ".highlight-y"make a contact button variable
+const highlightY = document.querySelectorAll(".highlight-y");
+const highlightP = document.querySelectorAll(".highlight-p");
+
+
+
+const contactModal = document.querySelector("#contact");
+const main = document.querySelector("main");
+//when the contact button is clicked, add the show class to the contact modal and the main section
+highlightY.forEach(button => {
+    button.addEventListener("click", function () {
+        contactModal.classList.add("show");
+        main.classList.add("show");
+    });
+});
+highlightP.forEach(button => {
+    button.addEventListener("click", function () {
+        contactModal.classList.add("show");
+        main.classList.add("show");
+    });
+});
+
+
+
+
+
+
+//close the contact modal and remove the show class from the main section when the ".close" button is clicked
+const close = document.querySelector(".close");
+close.addEventListener("click", function () {
+    contactModal.classList.remove("show");
+    main.classList.remove("show");
+}
+);
+
+
